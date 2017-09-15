c752e0dc7f08e7098299359298a65386535ad713
Nelio Alves <acenelio@gmail.com>
2017-09-15 20:40:06 -0300

    Criando variaveis

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -15,6 +15,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class FeedPage {
 
+  public nomeUsuario:string = "Nelio Alves do Código";
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
